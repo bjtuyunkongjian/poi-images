65cc883ce23876f48c926b7a693044d786e019d1
sl204984 <17798520003@163.com>
2018-11-07 04:45:11 +0800

    git

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-var fs = require('fs');
+const fs = require('fs');
 const kind2Img = require('./poi-map');
 const OLDDIR = './images-chosen'; // 旧文件夹
 const NEWDIR = './images-kind'; // 新文件夹
 
-
-// 修改文件名称
-function _rename(oldPath, newPath) {
-  fs.rename(oldPath, newPath, function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
 function _callback(fileName) {
   const _oldPath = OLDDIR + '/' + fileName;
+  let flag = false;
   for (let key in kind2Img) {
     if (kind2Img[key] === fileName.substring(0, fileName.length - 4)) {
       const _newPath = NEWDIR + '/ic_map_' + key + '.png';
-      console.log(_newPath);
-      _rename(_oldPath, _newPath);
+      fs.writeFileSync(_newPath, fs.readFileSync(_oldPath));
+      flag = true;
     }
+  }
+  if (!flag) { // 判断未使用的文件
+    console.log(fileName);
   }
 }
 
 // 遍历目录得到文件信息
 function main(_path) {
-  var files = fs.readdirSync(_path);
+  const files = fs.readdirSync(_path);
 
   files.forEach(function (fileName) {
     if (fs.statSync(_path + '/' + fileName).isFile()) {
